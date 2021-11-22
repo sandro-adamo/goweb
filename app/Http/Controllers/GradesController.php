5de41a769d455a77019431c67eed877ac5afbf1c
Sandro Adamo <sandro@goeyewear.com.br>
2021-11-22 12:38:07 -0300

    a

--- app/Http/Controllers/GradesController.php
+++ app/Http/Controllers/GradesController.php
@@ -320,7 +320,7 @@ from (
 				left join saldos on saldos.curto = itens.id
 				
 				where itens.secundario not like '%semi%' and clasmod in ('linha a++', 'linha a+', 'linha a', 'novo')  
-                and itens.agrup like '$agrup' 
+                and itens.agrup like '$agrup' and itens.colmod like '2022%'
 				
                 and codgrife in ('AH','AT','BG','EV','JO','HI','SP','TC','JM','NG','GU','MM','ST','AM','MC','CT','BC','BV','SM') 
 			) as fim2
