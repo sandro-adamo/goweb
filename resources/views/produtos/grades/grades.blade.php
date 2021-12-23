56a4094f295d0cc719127e478991cc76cfaa9dd8
Sandro Adamo <sandro@goeyewear.com.br>
2021-12-22 22:10:16 -0300

    m

--- resources/views/produtos/grades/grades.blade.php
+++ resources/views/produtos/grades/grades.blade.php
@@ -14,9 +14,10 @@
 
   <div class="col-md-2">
     <div class="box box-widget">
-      <div class="box-header with-border" style="font-size:16px; padding: 3px 5px 3px 5px; margin-bottom: 0; vertical-align: top;">
+		
+      <div class="box-header with-border" style="font-size:10px; padding: 3px 5px 3px 5px; margin-bottom: 0; vertical-align: top;">
         <span class="text-bold">{{trim($catalogo->agrup)}}</span> 
-        </div>
+      </div>
           
       <div id="foto" align="center" style="margin-top:30px; min-height:160;height:160; top:30%; margin-bottom:0; padding-bottom:0;">
   
