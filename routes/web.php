738264bab25be74a45df73db7b309d940c488d91
Sandro Adamo <sandro@goeyewear.com.br>
2021-12-28 19:18:09 -0300

    a

--- routes/web.php
+++ routes/web.php
@@ -522,6 +522,8 @@ Route::get('/produtos/agregados/{modelo}', 'AgregadosController@listaAgregadosIt
 
 
 Route::get('/produtos/grades', 'GradesController@listaGrades');
+
+
 Route::get('/produtos/gradesitens/{modelo}', 'GradesController@listaGradesItens');
 Route::get('/produtos/gradescolecoes/{modelo}', 'GradesController@listaGradesColecoes');
 Route::get('/produtos/gradesmodelos/{modelo}', 'GradesController@listaGradesModelos');
@@ -529,6 +531,11 @@ Route::get('/produtos/gradescoldet/{modelo}', 'GradesController@listaGradesColde
 
 
 
+
+Route::get('/gradesmod', function () {
+    //return view('layout.home');
+    return view('produtos/grades/gradesmod');
+});
 
 Route::get('/gradesmod_painel', function () {
     //return view('layout.home');
