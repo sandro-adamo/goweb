0bb6f237a6e3e87c752bfc9514203fa3d005a8d9
Sandro Adamo <sandro@goeyewear.com.br>
2022-01-07 23:08:36 -0300

    a

--- resources/views/produtos/grade/detalhe.blade.php
+++ resources/views/produtos/grade/detalhe.blade.php
@@ -11,8 +11,66 @@
 
 @php
 $query = \DB::select("select * from itens where modelo = 'ah6254' ");
-
 $data = '2021-01-01';
+
+
+$modelos = \DB::select("select fornecedor, grife, codgrife, agrup, modelo,colecao,
+	sum(novos) novos, sum(aa) aa, sum(a) a, 
+	sum(itens) itens, sum(imediata) imediata, sum(futura) futura, sum(producao) producao, sum(esgotado) esgotado, 
+	sum(am3cores) am3cores, sum(b2cores) b2cores, sum(c1cor) c1cor, sum(d0cor) d0cor 
+from (
+
+	select fornecedor, grife, codgrife, agrup, colecao, modelo,
+	case when colecao = 'novo' then 1 else 0 end as novos,
+	case when colecao = 'aa' then 1 else 0 end as aa, 
+	case when colecao = 'a' then 1 else 0 end as a, 
+	sum(itens) itens, sum(imediata) imediata, sum(futura) futura, sum(producao) producao, sum(esgotado) esgotado, 
+	sum(am3cores) am3cores, sum(b2cores) b2cores, sum(c1cor) c1cor, sum(d0cor) d0cor 
+	from (
+
+		select fornecedor, grife, codgrife, agrup, modelo, clasmod, colmod, (itens) as itens, (imediata) imediata, (futura) futura, (producao) producao, (esgotado) esgotado,
+			case when imediata+futura >= 3 then 1 else 0 end as am3cores,
+			case when imediata+futura  = 2 then 1 else 0 end as b2cores,
+			case when imediata+futura  = 1 then 1 else 0 end as c1cor,
+			case when imediata+futura  = 0 then 1 else 0 end as d0cor,
+			
+			 case when colecao = 'novo' then 'novo'
+			 when colecao <> 'novo' and clasmod in ('LINHA A++','LINHA A+','LINHA A','NOVO') then 'aa'
+			 when colecao <> 'novo' and clasmod in ('LINHA A-') then 'a' else '' end as colecao
+			
+			
+		from(
+
+			select fornecedor, grife, codgrife, agrup, modelo, clasmod, colmod, colecao, sum(itens) as itens, sum(imediata) imediata, sum(futura) futura, sum(producao) producao, sum(esgotado) esgotado
+			
+			from (
+			 
+				select fornecedor, grife, codgrife, agrup, modelo, clasmod, colmod, colecao, 1 as itens,
+					case when ultstatus = 'ENTREGA IMEDIATA' then 1 else 0 end as imediata,
+					case when ultstatus like '%DIAS' then 1 else 0 end as futura,
+					case when ultstatus like '%PROD%' then 1 else 0 end as producao,
+					case when ultstatus like '%ESGOTADO%' then 1 else 0 end as esgotado
+				from (
+							
+					select case when fornecedor like 'kering%' then 'kering' else 'go' end as fornecedor,
+					grife, codgrife, itens.agrup, itens.modelo, itens.secundario, colmod, clasmod, ultstatus,
+					case when (left(colmod,4) <= year(now()) and right(colmod,2) < month(now())) then 'lancado' else 'novo' end as colecao
+					from itens 
+					where itens.secundario not like '%semi%' and (clasmod like 'linha%' or clasmod like 'novo%') and codtipoitem = 006				 
+					and codgrife in ('AH','AI',  'AT','BG','EV','JO','HI','SP','TC','JM','NG','GU','MM','ST','AM','MC','CT','BC','BV','SM') 
+					and codtipoarmaz not in ('o')
+                    and agrup = 'ah02 - ana hickmann (rx)'
+				) as fim2
+			) as fim3 group by fornecedor, grife, codgrife, agrup, modelo, clasmod, colmod, colecao
+		) as fim4 
+	) as fim5 group by fornecedor, grife, codgrife, agrup, colecao, modelo
+) as fim6 group by fornecedor, grife, codgrife, agrup, modelo, colecao
+order by fornecedor, agrup, modelo");
+
+
+
+
+
 
 @endphp
 
@@ -32,35 +90,36 @@ $data = '2021-01-01';
 				<li  class="active"><a href="#Painel" data-toggle="tab">Painel</a></li>
 				<li><a href="#Tabela" data-toggle="tab">Tabela</a></li>
 				<li><a href="#Grade" data-toggle="tab">Grade</a></li>
-				<li><a href="#qualidade" data-toggle="tab">Qualidade</a></li>
-				<li><a href="#apontamentos" data-toggle="tab">Apontamentos</a></li>
-				<li><a href="#reprocesso" data-toggle="tab">Reprocessos</a></li>
-				<li><a href="#data_producao" data-toggle="tab">Data producao</a></li>
+				<li><a href="#Representantes" data-toggle="tab">Representantes</a></li>
+				<li><a href="#Mediasugest" data-toggle="tab">Mediasugest</a></li>
+				<li><a href="#Timeline_lancamentos" data-toggle="tab">Timeline_lancamentos</a></li>
+				<li><a href="#Estoques" data-toggle="tab">Estoques</a></li>
+				<li><a href="#Clientes" data-toggle="tab">Clientes</a></li>
 				
 
-<!-- aba geral inicio -->				
+				
 <div class="tab-content">
+	
+	
+	
+<!-- aba geral inicio -->	
 <div class="active tab-pane" id="Painel">
 <div class="col-md-12">
+<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
 
- <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
 
-
-<div class="col-md-10">
-<span class="lead">Modelos</span>
 	
+<div class="col-md-12">
 		
-		@foreach ($query as $catalogo)
+		@foreach ($modelos as $catalogo)
 		
-
-<div class="row">
-
       <div class="col-md-2">
         <div class="box box-widget">
-          <div  class="box-header with-border" style="font-size:14px; padding: 12px 10px 12px 10px;"> 
-          <b><a href="/painel/{{$catalogo->agrup}}/{{$catalogo->modelo}}/{{$catalogo->modelo}}" class="text-black">{{$catalogo->modelo}}</a></b>
-          <span class="pull-right">  {{$catalogo->modelo}}</span>
-			  <span class="pull-right">{{$catalogo->colmod}}</span>
+         
+			<div  class="box-header with-border" style="font-size:14px; padding: 12px 10px 12px 10px;"> 
+          		<b><a href="/painel/{{$catalogo->agrup}}/{{$catalogo->modelo}}/{{$catalogo->modelo}}" class="text-black">{{$catalogo->modelo}}</a></b>
+          		<span class="pull-right">  {{$catalogo->modelo}}</span>
+			 	<span class="pull-right">{{$catalogo->colecao}}</span>
 			</div>
 
 
@@ -70,102 +129,74 @@ $data = '2021-01-01';
         @endphp
 
           <div id="foto" align="center" style="min-height: 100px; max-height: 100px;">
-            <a href="" class="zoom" data-value="{{$catalogo->modelo}}"><img src="/{{$foto}}" class="img-responsive"></a>
-
-                  
+            <a href="" class="zoom" data-value="{{$catalogo->modelo}}"><img src="/{{$foto}}" class="img-responsive"></a>   
           </div>
 			
 			
 			 
-			<br>
-			<table class="table table-bordered" style="text-align: left;">
+		  <br>
+		  <table class="table table-bordered" style="text-align: left;">
           <tr>
-			  <td class=""><i class="fa fa-chain"></i><b> Itens</td>
-            <td class="">{{$catalogo->valortabela}} </b>
-				</td>
-          </tr> </table>
-      
-              
-				
-
-		
+			<td class=""><i class="fa fa-chain"></i><b> Itens</td>
+            <td class="">{{$catalogo->imediata}} </b> </td>
+          </tr> 
+		  </table>
+ 
 		
 		<br><br>
 		 <a title="Com estoque sem vinculo" href="" class="zoom" data-value="{{$catalogo->modelo}}"><i class="fa fa-chain-broken text-red fa-3x" style="position:absolute; top:200px; left:5%; opacity:0.8;" ></i> </a>
 				
 			
-            
-		
-		
 		 <a title="Revisar item"  href="" class="zoom" data-value="{{$catalogo->modelo}}"><i class="fa fa-warning text-orange fa-2x" style="position:absolute; top:50px; left:5%; opacity:0.8;" ></i></a>
 		
 
-		
-<div class="box-body">
-           <!-- linha 452--> 
 			  
-			  
-<div class="row">
-    <div class="col-md-12">
-        <table width="100%" style="text-align: center;">
-            <tr>
+					
+							<table width="100%" style="text-align: center;">
+								<tr>
 
-			 <td>
-                    <table class="table table-condensed table-bordered table2"  style="text-align: center;">
-                        <tr>
-                            <td align="center"><img src="/img/brasil.png" width="15"></i></td>
-                            <td>{{number_format($catalogo->valortabela)}}</td>
-                        </tr>
-                    </table>
+								 <td>
+										<table class="table table-condensed table-bordered table2"  style="text-align: center;">
+											<tr>
+												<td align="center"><img src="/img/brasil.png" width="15"></i></td>
+												<td>{{number_format($catalogo->imediata)}}</td>
+											</tr>
+										</table>
 
-                </td>
-                <td>
-                    <table class="table table-condensed table-bordered table2" style="text-align: center;">
-                        <tr>
-                            <td><i class="fa fa-plane text-blue"></i></td>
-                            <td>{{number_format($catalogo->valortabela)}}</td>
-                        </tr>
-                    </table>
-                </td>
-                <td>
-                    <table class="table table-condensed table-bordered table2" style="text-align: center;">
-                        <tr>
-                            <td><i class="fa fa-industry text-purple"></i></td>
-                            <td>{{number_format($catalogo->valortabela)}}</td>
-                        </tr>
-                    </table>
-                </td>
-                <td>
-                    <table class="table table-condensed table-bordered table2" style="text-align: center;">
-                        <tr>
-                            <td><i class="fa fa-warning text-yellow"></i></td>
-                            <td>{{number_format($catalogo->valortabela)}}</td>
-                        </tr>
-                    </table>
-                </td>
- 				
-            </tr>
+									</td>
+									<td>
+										<table class="table table-condensed table-bordered table2" style="text-align: center;">
+											<tr>
+												<td><i class="fa fa-plane text-blue"></i></td>
+												<td>{{number_format($catalogo->imediata)}}</td>
+											</tr>
+										</table>
+									</td>
+									<td>
+										<table class="table table-condensed table-bordered table2" style="text-align: center;">
+											<tr>
+												<td><i class="fa fa-industry text-purple"></i></td>
+												<td>{{number_format($catalogo->imediata)}}</td>
+											</tr>
+										</table>
+									</td>
+									<td>
+										<table class="table table-condensed table-bordered table2" style="text-align: center;">
+											<tr>
+												<td><i class="fa fa-warning text-yellow"></i></td>
+												<td>{{number_format($catalogo->imediata)}}</td>
+											</tr>
+										</table>
+									</td>
 
+								</tr>
+							</table>
 
-
-
-        </table>
-
-
-    </div>
-</div>
-			  
-			  
-			  
-
-          </div>
-        </div>
-      </div>
-      @endforeach
-
+	</div> 
+  </div>
      
-
-    </div>
+	@endforeach
+	
   </div>
 
 
@@ -214,10 +245,10 @@ $data = '2021-01-01';
 		</tr>
 	</thead>
 	<tbody>
-		@foreach ($query as $catalogo)
+		@foreach ($modelos as $catalogo)
 
 		@php
-		switch ($catalogo->secundario) {
+		switch ($catalogo->modelo) {
 			case 'entradas':
 			$formato = 'fa fa-plus-square text-green';
 			
@@ -232,17 +263,10 @@ $data = '2021-01-01';
 		}
 		@endphp
 		<tr>
-			<td align="left" class="{{$formato}}"> {{$catalogo->secundario}}</td>
+			<td align="left" class="{{$formato}}"> {{$catalogo->imediata}}</td>
 			<td align="left">  <a href="/painel/{{$catalogo->agrup}}/{{$catalogo->modelo}}">{{$catalogo->modelo}}</a></td>
-			<td align="left"> {{$catalogo->secundario}}</td>
-			<td align="left"> {{$catalogo->clasmod}}</td>
-			<td align="left"> {{$catalogo->colmod}}</td>
-			<td align="left"> {{$catalogo->valortabela}}</td>
-			<td align="left"> {{$catalogo->genero}}</td>
-			<td align="left"> {{$catalogo->idade}}</td>
-			<td align="left"> {{$catalogo->tamolho}}</td>
-			<td align="left"> {{$catalogo->fixacao}}</td>
-			<td align="left"> {{$catalogo->fornecedor}}</td>
+			<td align="left"> {{$catalogo->imediata}}</td>
+			
 		</tr>
 		@endforeach
 	</tbody>
@@ -594,7 +618,7 @@ $data = '2021-01-01';
 				<tr>
 					<td><i class="fa fa-th"></i> Fix</td>
 					<td>TOTAL</td>
-					<td>{{$catalogo->valortabela}} </td>
+					<td>{{$catalogo->imediata}} </td>
 					<td>{{$catalogo->codgrife}} </td>
 					<td>{{$catalogo->codgrife}} </td>
 					<td>{{$catalogo->codgrife}} </td>
@@ -1585,7 +1609,7 @@ where secundario LIKE  'ah6254 a01%' and date(historicos.created_at) = '$data->d
 				  				  				  
 				  				  				  				  
 				  				  				  				  				  				  
-    </div>
+</div> <!-- div tab content -->
 
   </div>
 
