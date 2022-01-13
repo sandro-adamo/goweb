5ae5205930fe3a2be3b607b63018e3ba61297011
Sandro Adamo <sandro@goeyewear.com.br>
2022-01-13 01:57:49 -0300

    a

--- resources/views/dashboards/estoque/lista.blade.php
+++ resources/views/dashboards/estoque/lista.blade.php
@@ -22,8 +22,8 @@ from (
 	sum(penultimo) penultimo, sum(antipenultimo) antipenultimo
 	from (
 
-		select case when sint.colmod_a = ' <= 2016' then left(sint.colmod,8) else left(sint.colmod,4) end as anomod, 
-		case when sint.colmod_a < year(now()) then left(sint.colmod,4) else sint.colmod_a end as colmod_a, sint.genero,
+		select case when sint.colmod_a = ' <= 2016' then left(sint.colmod_a,8) else left(sint.colmod_a,4) end as anomod, 
+		case when sint.colmod_a < year(now()) then left(sint.colmod_a,4) else sint.colmod_a end as colmod_a, sint.genero,
 		sum(orcamento_liber+orcamento_bloq) orca, sum(disponivel) disp, sum(cet) cet, sum(etq) etq, sum(cep) prod, sum(etq_total_vendas)etq_total_vendas, sum(compras) compras, 
 		sum(mostruarios) most, sum(reservas_estrat) reservas_estrat, sum(manutencao) manut
 		,sum(atual) atual,sum(ultimo) ultimo, sum(penultimo) penultimo, sum(antipenultimo) antipenultimo, sum(itens_disp) itens_disp
