f430fb4a8d4d39a88731c4b4d9e9fa2b1dadf2a0
Sandro Adamo <sandro@goeyewear.com.br>
2022-02-07 14:36:40 -0300

    a

--- resources/views/dashboards/representante/lista.blade.php
+++ resources/views/dashboards/representante/lista.blade.php
@@ -87,7 +87,8 @@ select distinct id id_rep
 				  </a>
 
 				@php	  
-				 $query_2 = \DB::select("select distinct coddir, dir from carteira where status = 1 and dt_fim >= now() and cli_ativo = 1 ");
+				 $query_2 = \DB::select("select distinct coddir, dir from carteira 
+				 where status = 1 and dt_fim >= now() and cli_ativo = 1 ");
 				@endphp	  
 				  <ul class="treeview-menu">
 				  @foreach ($query_2 as $query2)
@@ -119,8 +120,8 @@ select distinct id id_rep
 		<!-- for rep -->			
 								@php
 								$query_4 = \DB::select("
-							  select distinct rep, nome from carteira cart left join addressbook ab on ab.id = rep 
-							  where status = 1 and dt_fim >= now() and codsuper = '$query3->codsuper' and cli_ativo = 1"); 
+	 						    select distinct rep, nome from carteira cart left join addressbook ab on ab.id = rep 
+							    where status = 1 and dt_fim >= now() and codsuper = '$query3->codsuper' and coddir = '$query2->coddir' and cli_ativo = 1"); 
 								@endphp
 
 								<ul class="treeview-menu">
@@ -152,89 +153,6 @@ select distinct id id_rep
 
 
 
-
-
-
-
-
-
-<div class="row"> 	
-	<div class="col-md-5">	
-		<div class="box box-body">		
-
-			<ul class="sidebar-menu tree" data-widget="tree">
-				<li class="header">Diretoria</li>
-
-
-				  <li class="treeview">
-				  <a href="https://adminlte.io/themes/AdminLTE/index2.html#">
-					<i class="fa fa-share"></i> <span>Total</span>
-					<span class="pull-right-container">
-					  <i class="fa fa-angle-left pull-right"></i>
-					</span>
-				  </a>
-
-				@php	  
-				 $query_2 = \DB::select("select distinct grife from carteira where status = 1 and dt_fim >= now() and cli_ativo = 1 ");
-				@endphp	  
-				  <ul class="treeview-menu">
-				  @foreach ($query_2 as $query2)
-
-		<!-- for diretor -->  			  
-					<li class="treeview">
-					  <a href="https://adminlte.io/themes/AdminLTE/index2.html#"><i class="fa fa-circle-o"></i> {{$query2->grife}}
-						<span class="pull-right-container">
-						 20 <i class="fa fa-angle-left pull-right"></i>
-						</span>
-					  </a>
-
-		<!-- for supervisor -->
-					@php
-					$query_3 = \DB::select("select distinct codsuper, sup from carteira where status = 1 and dt_fim >= now() and grife = '$query2->grife' and cli_ativo = 1"); 
-					@endphp
-
-
-					  <ul class="treeview-menu">
-					  @foreach ($query_3 as $query3)
-
-						  <li class="treeview">
-						  <a href="https://adminlte.io/themes/AdminLTE/index2.html#"><i class="fa fa-circle-o"></i> {{$query3->sup}}
-							<span class="pull-right-container">
-							  <i class="fa fa-angle-left pull-right"></i>
-							</span>
-						  </a>
-
-		<!-- for rep -->			
-								@php
-								$query_4 = \DB::select("
-							  select distinct rep, nome from carteira cart left join addressbook ab on ab.id = rep 
-							  where status = 1 and dt_fim >= now() and codsuper = '$query3->codsuper' and cli_ativo = 1"); 
-								@endphp
-
-								<ul class="treeview-menu">
-								@foreach ($query_4 as $query4)
-
-								<li><a href="https://adminlte.io/themes/AdminLTE/index2.html#"><i class="fa fa-circle-o"></i> {{$query4->rep}} {{$query4->nome}}</a>
-
-								@endforeach	
-
-								</ul>
-						  </li>
-
-						@endforeach    				  
-
-
-					  </ul>
-					</li>
-					@endforeach 
-			<!-- end for diretor -->		  
-
-				  </ul>
-				</li>
-			  </ul>
-		</div>
-	</div>
-</div>
 
 
 
