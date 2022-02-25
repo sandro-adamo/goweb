3dd64f08f3c3acf7b9433bef05cd3aaf30256efe
Sandro Adamo <sandro@goeyewear.com.br>
2022-02-25 15:49:12 -0300

    a

--- app/Http/Controllers/ExemploController.php
+++ app/Http/Controllers/ExemploController.php
@@ -10,16 +10,28 @@ class ExemploController extends Controller
 {
     public function index(){
 
-        $pedidos = PedidoJDE::orderBy('id', 'desc')->paginate(15);
-
+        $pedidos = PedidopJDE::orderBy('id', 'desc')->paginate(15);
+		
         return view('exemplo.index', compact('pedidos'));
 
     }
 
     public function store(Request $request){
         
-        $exemplo = Exemplo::create($request->all());
-
+       // $exemplo = Exemplo::create($request->all());
+		
+		$exemplo = new Exemplo();
+		
+		$exemplo->campo1 = $request->campo1;
+		$exemplo->save();
+		
+		
+		dd($exemplo);
+		
+		$exemplo = Exemplo::find($id);
+		$exemplo->campo1 = $request->campo1;
+		$exemplo->save();
+		
         return redirect()->route('exemplo.index');
 
     }
