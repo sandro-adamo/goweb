5bde50e58f03f75bf35c23504da1ef123caa84a7
riosdani <98535297+riosdani@users.noreply.github.com>
2022-03-09 19:02:33 -0300

    mediavenda

--- resources/views/produtos/painel/info-vendas.blade.php
+++ resources/views/produtos/painel/info-vendas.blade.php
@@ -25,7 +25,9 @@
                     <table class="table table-condensed table-bordered table2" style="text-align: center;">
                         <tr>
                             <td><i class="fa fa-bar-chart text-blue" data-toggle="tooltip" data-placement="top" title="Média de Venda"></i></td>
-                            <!-- <td>{{number_format($catalogo->mediavenda)}}</td> -->
+                            @if($catalogo->mediavenda)
+                            <td>{{number_format($catalogo->mediavenda)}}</td>
+                            @endif
                         </tr>
                     </table>
                 </td>
