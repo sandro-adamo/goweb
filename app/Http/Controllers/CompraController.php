6fbcb57a3dad74cb1fd202e298d8ac7786bb81b1
Sandro Adamo <sandro@goeyewear.com.br>
2022-03-23 18:26:49 -0300

    a

--- app/Http/Controllers/CompraController.php
+++ app/Http/Controllers/CompraController.php
@@ -28,8 +28,8 @@ class CompraController extends Controller {
 				$insert = \DB::select("insert into pesquisa_naovenda (usuario, cliente, grife, motivo, atendimento, obs) 
 				values ($id_usuario, '$request->cliente', '$grife', '$motivo', '$request->atendimento', '$request->obs')");
 				
-				$insert2 = \DB::select("insert into compras_parcelas (numero,origem) 
-				values (99987, '$request->adiantamento')");
+				$insert2 = \DB::select("insert into compras_parcelas (numero,origem,tipo) 
+				values (99987, '$request->adiantamento', '$request->embarque')");
 				
 				}
 			}
