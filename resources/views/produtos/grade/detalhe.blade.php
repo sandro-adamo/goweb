519ff8ab20a1f9f053db8b4e22975b72db5f5222
Sandro Adamo <sandro@goeyewear.com.br>
2022-05-02 18:42:59 -0300

    a

--- resources/views/produtos/grade/detalhe.blade.php
+++ resources/views/produtos/grade/detalhe.blade.php
@@ -48,7 +48,7 @@ $data = '2021-01-01';
 
 $modelos = \DB::select("
 
-select ciclo, fornecedor, codgrife, codgrife grife, agrup, modelo, colecao, colmod, clasmod, genero, estilo, idade,
+select ciclo, fornecedor, codgrife, codgrife grife, agrup, modelo, colecao, colmod, clasmod, genero, estilo, idade, valortabela,
 sum(disponivel) disponivel, sum(beneficiamento) beneficiamento, sum(cet) cet, sum(cep) cep, sum(most) most, sum(total) total,
 sum(atual)atual, sum(ultimo)ultimo, sum(mes_sem)mes_sem, sum(mes_ano)mes_ano, sum(qtde_total) qtde_total,
 sum(novos) novos, sum(aa) aa, sum(a) a, 
@@ -56,7 +56,7 @@ sum(itens) itens, sum(imediata) imediata, sum(futura) futura, sum(producao) prod
 sum(am3cores) am3cores, sum(b2cores) b2cores, sum(c1cor) c1cor, sum(d0cor) d0cor 
 from (
 
-	select ciclo, fornecedor, codgrife, agrup, colecao, modelo, colmod, clasmod, genero, estilo, idade,
+	select ciclo, fornecedor, codgrife, agrup, colecao, modelo, colmod, clasmod, genero, estilo, idade, valortabela,
 	case when colecao = 'novo' then 1 else 0 end as novos, case when colecao = 'aa' then 1 else 0 end as aa, case when colecao = 'a' then 1 else 0 end as a, 
 	sum(itens) itens, sum(imediata) imediata, sum(futura) futura, sum(producao) producao, sum(am3cores) am3cores, sum(b2cores) b2cores, sum(c1cor) c1cor, sum(d0cor) d0cor,
     sum(disponivel) disponivel, sum(beneficiamento) beneficiamento, sum(cet) cet, sum(cep) cep, sum(most) most, sum(total) total,
@@ -64,7 +64,7 @@ from (
 
 	from (
     
-		select ciclo, fornecedor, codgrife, agrup, modelo, clasmod, colmod, genero, estilo, idade, (itens) as itens, (imediata) imediata, (futura) futura, (producao) producao, 
+		select ciclo, fornecedor, codgrife, agrup, modelo, clasmod, colmod, genero, estilo, idade, valortabela, (itens) as itens, (imediata) imediata, (futura) futura, (producao) producao, 
         (disponivel) disponivel, (beneficiamento) beneficiamento, (cet) cet, (cep) cep, (most) most, (total) total,
         atual, ultimo, mes_sem, mes_ano, qtde_total,
 
@@ -78,7 +78,7 @@ from (
 		from(
 
 
-			select ciclo, fornecedor, codgrife, agrup, modelo, clasmod, colmod, colecao, genero, estilo, idade,
+			select ciclo, fornecedor, codgrife, agrup, modelo, clasmod, colmod, colecao, genero, estilo, idade, valortabela,
 			count(secundario) as itens, sum(itens_imed) imediata, sum(itens_trans) futura, sum(itens_prod) producao,
             sum(disponivel) disponivel, sum(beneficiamento) beneficiamento, sum(cet) cet, sum(cep) cep, sum(most) most, sum(total) total,
             sum(atual)atual, sum(ultimo)ultimo, sum(mes_sem)mes_sem, sum(mes_ano)mes_ano, sum(qtde_total_jde) qtde_total
@@ -93,6 +93,8 @@ from (
 
 				(select idade from itens iclas where left(agrup,5) = '$agrup'  and  iclas.id = id_item and idade  not in ('','.') order by idade limit 1) idade,
                 
+				(select valortabela from itens iclas where left(agrup,5) = '$agrup'  and  iclas.id = id_item and valortabela  not in ('','.') order by valortabela desc limit 1) valortabela,
+
 				itens_imed, itens_trans, itens_prod, 
                 (disponivel) disponivel, (conferencia+montagem) beneficiamento, (cet) cet, (etq+cep) cep, (mostruarios) most,
 				(disponivel+conferencia+montagem+cet+etq+cep) total, 
@@ -104,11 +106,11 @@ from (
 				and codgrife in ('GO','AH','AI','FE','AT','BG','EV','JO','HI','SP','TC','JM','NG','GU','MM','ST','AM','MC','CT','BC','BV','SM','CH') 
 				and left(agrup,5) = '$agrup' 
                 
-			) as base group by ciclo, fornecedor, codgrife, agrup, modelo, clasmod, colmod, colecao, genero, estilo, idade
+			) as base group by ciclo, fornecedor, codgrife, agrup, modelo, clasmod, colmod, colecao, genero, estilo, idade, valortabela
 		) as base1 
 	) as base2 $where1
-    group by ciclo, fornecedor, codgrife, agrup, colecao, modelo, colmod, clasmod, genero, estilo, idade
-) as base3 group by ciclo, fornecedor, codgrife, agrup, modelo, colecao, colmod, clasmod, genero, estilo, idade
+    group by ciclo, fornecedor, codgrife, agrup, colecao, modelo, colmod, clasmod, genero, estilo, idade, valortabela
+) as base3 group by ciclo, fornecedor, codgrife, agrup, modelo, colecao, colmod, clasmod, genero, estilo, idade, valortabela
 order by modelo
 
 
@@ -140,7 +142,8 @@ order by modelo
 				<li><a href="#Mediasugest" data-toggle="tab">Mediasugest</a></li>
 				<li><a href="#Timeline_lancamentos" data-toggle="tab">Timeline_lancamentos</a></li>
 				<li><a href="#Estoques" data-toggle="tab">Estoques</a></li>
-				<li><a href="#Estoques" data-toggle="tab">Importacoes</a></li>
+				<li><a href="#Importacoes" data-toggle="tab">Importacoes</a></li>
+				<li><a href="#Compras" data-toggle="tab">Compras</a></li>
 				
 				
 
@@ -493,13 +496,28 @@ order by modelo
          
 			<div  class="box-header with-border" style="font-size:12px; padding: 15px 15px 15px 15px;"> 
 				
-          		<b><a href="/painel/{{$catalogo->agrup}}/{{$catalogo->modelo}}/{{$catalogo->modelo}}" class="text-black">{{$catalogo->modelo}}</a></b>
-          		<span class="pull-center"></span>
-			 	<span class="pull-right">{{$catalogo->clasmod}}</span>
+          		<b><a href="/painel/{{$catalogo->agrup}}/{{$catalogo->modelo}}/{{$catalogo->modelo}}" class="text-black">{{$catalogo->modelo}} </a></b>
+
 				<br>
-				<span class="pull-left">{{$catalogo->genero}}</span>
-				<span class="pull-left">  - {{$catalogo->idade}}</span>
-				<span class="pull-right">{{$catalogo->estilo}}</span>
+
+<table class="table table-condensed table-bordered table2" style="text-align: center;">
+	<tr>
+	<td>{{($catalogo->clasmod)}}</td>
+	<td>{{($catalogo->colmod)}}</td>
+	<td>{{number_format($catalogo->valortabela,2)}}</td>
+	</tr>
+	
+	<tr>
+	<td>{{($catalogo->idade)}}</td>
+	<td>{{($catalogo->estilo)}}</td>
+	<td>{{($catalogo->genero)}}</td>
+	</tr>
+</table>
+				
+
+          		
+			 	
+				
 			</div>
 
 
