8881cf8fb830a7488c6be0eb8e9e77757a99077b
Sandro Adamo <sandro@goeyewear.com.br>
2022-08-26 16:39:37 -0300

    a

--- resources/views/dashboards/importacao/insere_form.blade.php
+++ resources/views/dashboards/importacao/insere_form.blade.php
@@ -1332,6 +1332,45 @@ $query_4 = \DB::select("select itens.secundario, agrup, codgrife, modelo, 0 as r
               <!-- /.tab-pane -->
 				
 			
+					
+					
+
+          <!-- general form elements -->
+          <div class="tab-pane" id="documentos">
+           
+         
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form role="form">
+              <div class="box-body">
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Email address</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Password</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputFile">File input</label>
+                  <input type="file" id="exampleInputFile">
+
+                  <p class="help-block">Example block-level help text here.</p>
+                </div>
+                <div class="checkbox">
+                  <label>
+                    <input type="checkbox"> Check me out
+                  </label>
+                </div>
+              </div>
+              <!-- /.box-body -->
+
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </form>
+          </div>
+          <!-- /.box -->
 				
 					
 					
