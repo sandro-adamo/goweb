92d5ebb79c71702c5b572913ce58ca5cc463c168
riosdani <98535297+riosdani@users.noreply.github.com>
2022-10-06 17:21:55 -0300

    ajuste nome gestão

--- resources/views/layout/principal.blade.php
+++ resources/views/layout/principal.blade.php
@@ -17,7 +17,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Portal GO</title>
+  <title>Gestão GO</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
