f9de985925d5ad7da275f5ee4b9aec0f60ad9875
riosdani <98535297+riosdani@users.noreply.github.com>
2022-10-07 12:02:44 -0300

    ajuste gestão logo

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -4,7 +4,7 @@
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>GO</b></span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>GO</b>Web</span>
+      <span class="logo-lg"><b>Gestão</b>GO</span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top" style="background: #23364A !important;">
